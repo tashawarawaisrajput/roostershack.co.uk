8964687d9acdc6dd81e1ad8964087044df087c5c
tashawarawaisrajput <awaisraj8237@gmail.com>
2026-03-25 16:51:00 +0500

    changes

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -184,12 +184,14 @@ function CarouselPrevious({
       data-slot="carousel-previous"
       variant={variant}
       size={size}
+      // Maine yahan 'hidden' ko permanent add kar diya hai
       className={cn(
+        'hidden', 
         'absolute size-8 rounded-full',
         orientation === 'horizontal'
           ? 'top-1/2 -left-12 -translate-y-1/2'
           : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
-        className,
+        className
       )}
       disabled={!canScrollPrev}
       onClick={scrollPrev}
@@ -214,12 +216,14 @@ function CarouselNext({
       data-slot="carousel-next"
       variant={variant}
       size={size}
+      // Yahan bhi 'hidden' add kar diya hai
       className={cn(
+        'hidden',
         'absolute size-8 rounded-full',
         orientation === 'horizontal'
           ? 'top-1/2 -right-12 -translate-y-1/2'
           : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
-        className,
+        className
       )}
       disabled={!canScrollNext}
       onClick={scrollNext}
